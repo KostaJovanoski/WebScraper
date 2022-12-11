dd9975774128eb133ab99b458c7387a567d7410a
KostaJovanoski <jovanoskikosta50@gmail.com>
2022-12-11 23:46:07 +0100

    small fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,9 @@
-﻿export function HomePage(){
+﻿import {Link} from "react-router-dom";
+export function HomePage(){
     return (
         <div className={'container'}>
             <div className={'row'}>
-                <div className={'col-md-12 mt-3 mb-5'}>
+                <div className=<Link to={'/login'}>login Now.</Link>>
                     <h2 className={'display-6 text-center'}>Web Scraper</h2>
                 </div>
             </div>
